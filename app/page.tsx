9b0635adf6731599decb0db148dc69a076b03194
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2026-02-20 10:31:32 -0800

    fixed balanceIcon sizing error

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
           Supreme Court Opinion Assistant
         </h1>
         <div className="flex flex-col items-center">
-          <BalanceIcon sx={{ fontSize: 120 }} className="text-white" />
+          <BalanceIcon sx={{ fontSize: 65 }} className="text-white" />
           <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 font-semibold text-white shadow-inner shadow-white/10 data-true hover:bg-secondary mt-8">
             <Link href={`/chat`}>Enter Chat</Link>
           </Button>
